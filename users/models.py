263ba807e5cc9f386fb327fd214b7b24b72267d5
Ansup17174 <ansuported@op.pl>
2020-12-13 11:49:38 +0100

    shipping address model

--- users/models.py
+++ users/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField()
 
 
+User = get_user_model()
+
+
 class ShippingAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     street = models.CharField(max_length=200)
     number = models.CharField(max_length=20)
     post_code = models.CharField(max_length=6)
     city = models.CharField(max_length=100)
+    
