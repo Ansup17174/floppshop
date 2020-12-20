b02531fa15e8866834f1ca1a886e3bab54be5ef8
Ansup17174 <ansuported@op.pl>
2020-12-20 09:45:34 +0100

    Cart adding, removing and changing quantity of items added

--- shop/urls.py
+++ shop/urls.py
@@ -8,6 +8,6 @@ admin_item_router.register(r"items", AdminItemViewset, basename="shop-admin")
 urlpatterns = [
     path("admin/", include(admin_item_router.urls)),
     path("items/", UserItemView.as_view(), name="item_view"),
-    path("items/<int:pk>/", UserItemDetailView.as_view(), name="item_detail_view"),
+    path("items/<int:item_pk>/", UserItemDetailView.as_view(), name="item_detail_view"),
     path("order/", UserOrderView.as_view(), name="order_view")
 ]
--- shop/views.py
+++ shop/views.py
@@ -36,24 +36,38 @@ class UserItemDetailView(APIView):
         return Response(serializer.data, status=200)
 
     #add to cart
-    def post(self, request, pk):
-        item = get_object_or_404(Item, pk=pk, is_visible=True, is_available=True)
+    def post(self, request, item_pk):
+        item = get_object_or_404(Item, pk=item_pk, is_visible=True, is_available=True)
         user = request.user
         try:
-            quantity = int(request.query_params.get('quantity', 0))
+            quantity = int(request.query_params.get('quantity', " "))
         except ValueError:
             raise NotAcceptable("Invalid quantity")
-        if quantity > item.quantity or quantity <= 0:
+        if quantity == 0:
+            raise NotAcceptable("Invalid quantity")
+        if quantity > item.quantity:
             raise NotAcceptable("Invalid quantity")
         order, is_created = Order.objects.get_or_create(user=user, is_finished=False)
         if not is_created:
             cart = order.carts.get(item=item)
-            if cart.quantity + quantity > item.quantity:
+            if cart.quantity + quantity > item.quantity or cart.quantity + quantity < 0:
                 raise NotAcceptable("Invalid quantity")
+            if cart.quantity + quantity == 0:
+                order.quantity -= 1
+                order.total_price -= cart.total_price
+                try:
+                    with transaction.atomic():
+                        cart.delete()
+                        order.save()
+                    return Response({"detail": f"Removed {item.name} from cart"})
+                except IntegrityError:
+                    return Response({"detail": "Something went wrong when removing items from cart!"}, status=400)
             cart.quantity += quantity
             cart.total_price += item.price * quantity
             order.total_price += item.price * quantity
         else:
+            if quantity < 0:
+                raise NotAcceptable("Invalid quantity")
             cart = Cart.objects.create(order=order, item=item)
             cart.quantity += quantity
             cart.total_price += quantity * item.price
@@ -63,14 +77,13 @@ class UserItemDetailView(APIView):
             with transaction.atomic():
                 cart.save()
                 order.save()
-            return Response(
-                {
-                    "message": f"{quantity} items were added to cart for total price of {quantity*item.price}"
-                },
-                status=200
-            )
+                if quantity < 0:
+                    message = f"{-quantity} items were removed from cart for total price of {-quantity*item.price}"
+                else:
+                    message = f"{quantity} items were added to cart for total price of {quantity*item.price}"
+            return Response({"detail": message}, status=200)
         except IntegrityError:
-            return Response({"message": "Something went wrong when adding item to cart!"}, status=400)
+            return Response({"detail": "Something went wrong when adding item to cart!"}, status=400)
 
 
 class UserOrderView(APIView):
