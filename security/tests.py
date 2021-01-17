393c14706084638b75ae458f04d452f660930b61
Ansup17174 <ansuported@op.pl>
2021-01-17 13:41:53 +0100

    another user tests

--- security/tests.py
+++ security/tests.py
@@ -1,3 +1,192 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, override_settings
+from django.shortcuts import reverse
+from django.core import mail
+from django.contrib.auth import get_user_model
+from .serializers import CustomUserDetailsSerializer
+from datetime import timedelta
+import json
+import re
 
-# Create your tests here.
+User = get_user_model()
+
+
+class UserRegisterAndLoginTestCase(APITestCase):
+
+    def setUp(self):
+        self.email = "janusz@op.pl"
+        self.password = "krowa123"
+        self.first_name = "Januszaaa"
+        self.last_name = "Tracz"
+        self.phone = "666691337"
+        self.date_of_birth = "2000-01-04"
+
+    def test_register_new_user(self):
+        request_body = {
+            "email": self.email,
+            "password1": self.password,
+            "password2": self.password,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "phone": self.phone,
+            "date_of_birth": self.date_of_birth
+        }
+
+        response = self.client.post(reverse("rest_register"), request_body, format="json")
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data, {'detail': 'Verification e-mail sent.'})
+        key = re.search(r"MQ:[a-zA-Z0-9-_:]+", mail.outbox[0].body)
+        if not key:
+            raise AssertionError
+        email_confirmation_request = {"key": key.group(0)}
+        email_confirmation_response = self.client.post(
+            reverse("rest_verify_email"),
+            email_confirmation_request,
+            format="json"
+        )
+        self.assertEqual(email_confirmation_response.status_code, 200)
+        self.assertEqual(email_confirmation_response.data, {'detail': 'ok'})
+        self.assertTrue(User.objects.filter(email="janusz@op.pl").exists())
+
+    def test_user_login(self):
+        register_request = {
+            "email": self.email,
+            "password1": self.password,
+            "password2": self.password,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "phone": self.phone,
+            "date_of_birth": self.date_of_birth
+        }
+
+        self.client.post(reverse("rest_register"), register_request, format="json")
+        key = re.search(r"MQ:[a-zA-Z0-9-_:]+", mail.outbox[0].body)
+        if not key:
+            raise AssertionError
+        email_confirmation_request = {"key": key.group(0)}
+        self.client.post(
+            reverse("rest_verify_email"),
+            email_confirmation_request,
+            format="json"
+        )
+
+        login_request = {
+            "email": self.email,
+            "password": self.password
+        }
+        login_response = self.client.post(reverse("rest_login"), login_request, format="json")
+        self.assertEqual(login_response.status_code, 200)
+        user_from_db_serializer = CustomUserDetailsSerializer(User.objects.get(email=self.email))
+        self.assertEqual(login_response.data.get("user"), user_from_db_serializer.data)
+        self.assertEqual(login_response.cookies.get("floppauth").value, login_response.data.get("access_token"))
+
+
+class UserTestCase(APITestCase):
+
+    def setUp(self):
+        self.email = "janusz@op.pl"
+        self.password = "krowa123"
+        self.first_name = "Januszaaa"
+        self.last_name = "Tracz"
+        self.phone = "666691337"
+        self.date_of_birth = "2000-01-04"
+        request_body = {
+            "email": self.email,
+            "password1": self.password,
+            "password2": self.password,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "phone": self.phone,
+            "date_of_birth": self.date_of_birth
+        }
+        self.client.post(reverse("rest_register"), request_body, format="json")
+        key = re.search(r"MQ:[a-zA-Z0-9-_:]+", mail.outbox[0].body)
+        if not key:
+            raise AssertionError
+        email_confirmation_request = {"key": key.group(0)}
+        self.client.post(
+            reverse("rest_verify_email"),
+            email_confirmation_request,
+            format="json"
+        )
+        login_request = {
+            "email": self.email,
+            "password": self.password
+        }
+        self.client.post(reverse("rest_login"), login_request, format="json")
+
+    @override_settings(
+        SIMPLE_JWT={
+            'ACCESS_TOKEN_LIFETIME': timedelta(seconds=1),
+            'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+            'ROTATE_REFRESH_TOKENS': True
+        }
+    )
+    def test_user_token_expired(self):
+        login_request = {
+            "email": self.email,
+            "password": self.password
+        }
+        login_response = self.client.post(reverse("rest_login"), login_request, format="json")
+        self.assertEqual(login_response.status_code, 200)
+        self.assertEqual(
+            login_response.cookies.get("floppauth").value,
+            login_response.data.get("access_token")
+        )
+
+        user_details_response = self.client.get(reverse("rest_user_details"))
+        print(user_details_response.status_code, user_details_response.data)
+
+    def test_user_change_password(self):
+        request_body = {
+            "old_password": self.password,
+            "new_password1": "nowehaslo1337",
+            "new_password2": "nowehaslo1337"
+        }
+        response = self.client.post(reverse("rest_password_change"), request_body, format="json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {'detail': 'New password has been saved.'})
+        user = User.objects.get(email="janusz@op.pl")
+        self.assertTrue(user.check_password("nowehaslo1337"))
+        self.assertFalse(user.check_password(self.password))
+
+    def test_user_change_password_invalid(self):
+        request_body = {
+            "old_password": self.password,
+            "new_password1": "nowehaslo1337",
+            "new_password2": "cosinnego"
+        }
+        response = self.client.post(reverse("rest_password_change"), request_body, format="json")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(json.dumps(response.data), json.dumps({
+            "new_password2": ["The two password fields didn\u2019t match."]
+        }))
+
+    def test_user_reset_password(self):
+        reset_request = {"email": self.email}
+        reset_response = self.client.post(reverse("rest_password_reset"), reset_request, format="json")
+        self.assertEqual(reset_response.status_code, 200)
+        self.assertEqual(reset_response.data, {"detail": "Password reset e-mail has been sent."})
+        body = mail.outbox[-1].body
+        uid_token = re.search(r"/reset/([\d\w]+)/([\w\d-]+)", body)
+        confirm_request = {
+            "uid": uid_token.group(1),
+            "token": uid_token.group(2),
+            "new_password1": "qgkghdkgjd",
+            "new_password2": "qgkghdkgjd"
+        }
+        confirm_response = self.client.post(
+            reverse("rest_password_reset_confirm"),
+            confirm_request,
+            format="json"
+        )
+        self.assertEqual(confirm_response.status_code, 200)
+        self.assertEqual(confirm_response.data, {"detail": "Password has been reset with the new password."})
+        login_request = {
+            "email": self.email,
+            "password": "qgkghdkgjd"
+        }
+        login_response = self.client.post(reverse("rest_login"), login_request, format="json")
+        self.assertEqual(login_response.status_code, 200)
+        self.assertEqual(login_response.cookies.get("floppauth").value, login_response.data.get("access_token"))
+
+
