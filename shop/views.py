bd576cd49b2977915ed1f5543e50cb7f20f6a727
Ansup17174 <ansuported@op.pl>
2021-01-18 09:00:04 +0100

    no need to refactor cart function

--- shop/views.py
+++ shop/views.py
@@ -50,27 +50,6 @@ class UserItemDetailView(APIView):
 
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def add_item_to_existing_cart(self, order, item, quantity):
-        cart = order.carts.get(item=item)
-        if cart.quantity + quantity > item.quantity or cart.quantity + quantity < 0:
-            raise ValidationError({"detail": "Invalid quantity"})
-        if cart.quantity + quantity == 0:
-            order.quantity -= 1
-            order.total_price -= cart.total_price
-            try:
-                with transaction.atomic():
-                    cart.delete()
-                    if order.quantity:
-                        order.save()
-                    else:
-                        order.delete()
-                return Response({"detail": f"Removed {item.name} from cart"})
-            except IntegrityError:
-                return Response({"detail": "Something went wrong when removing items from cart!"}, status=400)
-        cart.quantity += quantity
-        cart.total_price += item.price * quantity
-        order.total_price += item.price * quantity
-
     def get(self, request, item_pk):
         item = get_object_or_404(Item, pk=item_pk, is_visible=True)
         serializer = ItemSerializer(item)
