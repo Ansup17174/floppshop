cc84cd1dbebd79ab23c1c68a9ffffc738be9af68
Ansup17174 <ansuported@op.pl>
2021-01-20 09:54:07 +0100

    wrote shop get items test and removed unnecessary users urlconf

--- floppshopproject/urls.py
+++ floppshopproject/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("django.contrib.auth.urls")),
-    path("user/", include('users.urls')),
     path("auth/", include("security.urls")),
     path("shop/", include("shop.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/tests.py
+++ shop/tests.py
@@ -1,6 +1,7 @@
 from rest_framework.test import APITestCase
 from django.shortcuts import reverse
 from .models import Order, Cart, Item, ItemImage, PayUNotification
+from .serializers import ItemSerializer
 from decimal import Decimal
 
 
@@ -21,3 +22,24 @@ class ShopTestCase(APITestCase):
             discount_price=Decimal("2.19"),
             is_discount=True
         )
+        self.cat_litterbox = Item.objects.create(
+            name="Litter box",
+            price=Decimal("21.99"),
+            quantity=0,
+            description="Litter box for cats",
+            is_available=False
+        )
+        self.catnip = Item.objects.create(
+            name="Catnip",
+            price=Decimal("25.00"),
+            quantity=20,
+            description="Drug for cats",
+            is_available=False,
+            is_visible=False
+        )
+
+    def test_get_items_view(self):
+        response = self.client.get(reverse("item_view"))
+        self.assertEqual(response.status_code, 200)
+        items_serializer = ItemSerializer(Item.objects.filter(is_available=True, is_visible=True), many=True)
+        self.assertEqual(response.data, items_serializer.data)
