9327405d3e2e52a6fc38d590c8e9594211264cb9
Ansup17174 <ansuported@op.pl>
2021-01-24 19:30:58 +0100

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,10 @@
-import axios from 'axios';
-import {useState} from 'react';
-
+import Header from './components/Header';
 
 
 const App = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [items, setItems] = useState([]);
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    const url = `http://localhost:8000/shop/items/?search=${inputValue}`;
-    axios.get(url)
-    .then(response => {
-      setItems(response.data);
-    })
-    .catch(error => {
-      console.log(error.response);
-      console.log(error.status);
-    })
-  };
-
-
-  
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={inputValue} onChange={e => setInputValue(e.target.value)}/>
-        <input type="submit" value="Search items by name"/>
-      </form>
-      <div>
-        {items.map(item => (
-          <div key={item.id}>
-            <h3>Name: {item.name}</h3>
-            <h3>Price: {item.price}</h3>
-            <h3>Description: {item.description}</h3>
-          </div>
-        ))}
-      </div>
+      <Header />
     </div>
   );
 };
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+
+
+const Header = () => {
+    return (
+    <nav className="navbar">
+        <a href="#" className="logo">Floppshop</a>
+        <ul className="nav-links">
+            <li className="nav-item">Contact</li>
+            <li className="nav-item">Register</li>
+            <li className="nav-item">Log in</li>
+        </ul>
+    </nav>
+    );
+};
+
+export default Header;
