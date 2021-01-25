2962ec82b7bcc4f214e87dc9884eb8cb741a0823
Ansup17174 <ansuported@op.pl>
2021-01-25 16:50:17 +0100

    layout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,14 +2,16 @@
 
 const Header = () => {
     return (
-    <nav className="navbar">
-        <a href="#" className="logo">Floppshop</a>
-        <ul className="nav-links">
-            <li className="nav-item">Contact</li>
-            <li className="nav-item">Register</li>
-            <li className="nav-item">Log in</li>
-        </ul>
-    </nav>
+    <div className="nav-wrapper">
+        <nav className="navbar">
+            <a href="#" className="logo">Floppshop</a>
+            <ul className="nav-links">
+                <li className="nav-item">Contact</li>
+                <li className="nav-item">Register</li>
+                <li className="nav-item">Log in</li>
+            </ul>
+        </nav>
+    </div>
     );
 };
 
