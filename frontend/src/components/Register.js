a938bc824cfea055bcf35df36ef21549d21aa1fe
Ansup17174 <ansuported@op.pl>
2021-01-26 15:46:13 +0100

    user can register

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {useState} from 'react';
 
 
@@ -11,13 +12,30 @@ const RegisterPage = () => {
         phone: "",
         dateOfBirth: "",
         password1: "",
+        password2: "",
         responseOk: false,
-        responseErrors: []
+        responseErrors: {}
     });
 
 
     const handleSubmit = e => {
         e.preventDefault();
+        const url = "http://localhost:8000/auth/registration/";
+        axios.post(url, {
+            email: formState.email,
+            first_name: formState.firstName,
+            last_name: formState.lastName,
+            phone: formState.phone,
+            date_of_birth: formState.dateOfBirth,
+            password1: formState.password1,
+            password2: formState.password2
+        })
+        .then(response => {
+            setFormState({...formState, responseOk: true});
+        })
+        .catch(error => {
+            setFormState({...formState, responseErrors: error.response.data, responseOk: false});
+        });
     };
 
 
@@ -26,9 +44,6 @@ const RegisterPage = () => {
         <form className="register-form" onSubmit={handleSubmit}>
             <h1>Registration</h1>
             <div className={formState.responseOk ? "register-success" : "hidden"}>Verification e-mail sent</div>
-            {formState.responseErrors.map((error, index) => (
-                <div className="register-fail">{error}</div>
-            ))}
             <input type="email" id="email" value={formState.email} placeholder="E-mail" onChange={e => setFormState({...formState, email: e.target.value})}/>
             <input type="text" value={formState.firstName} placeholder="First name" onChange={e => setFormState({...formState, firstName: e.target.value})}/>
             <input type="text" value={formState.lastName} placeholder="Last name" onChange={e => setFormState({...formState, lastName: e.target.value})}/>
@@ -41,7 +56,7 @@ const RegisterPage = () => {
                  onChange={e => setFormState({...formState, dateOfBirth: e.target.value})}/>
             <input type="password" value={formState.password1} placeholder="Password" onChange={e => setFormState({...formState, password1: e.target.value})}/>
             <input type="password" value={formState.password2} placeholder="Password again" onChange={e => setFormState({...formState, password2: e.target.value})}/>
-            <input className={formState.isValid ? "register-button" : ""} type="submit" value="Register" placeholder="Phone number"/>
+            <input className="register-button" type="submit" value="Register" placeholder="Phone number"/>
         </form>
     </div>
     );
