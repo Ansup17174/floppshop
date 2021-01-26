6c3d267eecfccb2368f71c3caa824ec0bf22974c
Ansup17174 <ansuported@op.pl>
2021-01-26 12:23:19 +0100

    not found on list

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -3,18 +3,18 @@
 const ItemList = ({items}) => {
     return (
         <div className="main-page">
-            {items.map((item, index) => (
+            {items.length ? items.map((item, index) => (
                 <div className="item" key={index}>
                     <img src={item.images[0] ? item.images[0] : "https://i.stack.imgur.com/y9DpT.jpg"} alt=""/>
                     <div className="item-info">
                         <div className="item-name">{item.name}</div>
                         <div className="item-description">{item.description}</div>
                         <div className="item-quantity">In-stock: {item.quantity}</div>
-                        {item.is_available || item.quantity <= 0 ? <div className="item-is-available">Available</div> : <div className="item-not-available">Not available</div>}
+                        {item.is_available || item.quantity <=0 ? <div className="item-is-available">Available</div> : <div className="item-not-available">Not available</div>}
                     </div>
                     <div className="item-price">{item.price}zł</div>
                 </div>
-            ))}
+            )) : <div>xD</div>}
         </div>
     );
 };
