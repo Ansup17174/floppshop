79df7f8e88d3e45fc34143101c56252c184d7c91
Ansup17174 <ansuported@op.pl>
2021-01-30 15:14:35 +0100

    add item to cart

--- frontend/src/components/ItemDetails.js
+++ frontend/src/components/ItemDetails.js
@@ -4,8 +4,16 @@ import {useParams, useHistory} from 'react-router-dom';
 
 const ItemDetails = () => {
 
-    const [item, setItem] = useState();
+    const [item, setItem] = useState({
+        name: "",
+        description: "",
+        quantity: "",
+        price: 0,
+
+    });
     const [quantity, setQuantity] = useState(1);
+    const [response, setResponse] = useState(false);
+    const [error, setError] = useState({});
     const { id } = useParams();
     const history = useHistory();
 
@@ -21,13 +29,19 @@ const ItemDetails = () => {
     }, []);
 
     const changeQuantity = e => {
-        if (e.target.value > 0) {
-            setQuantity(e.target.value);
+        if (e.target.value > 0 && e.target.value <= item.quantity) {
+            setQuantity(Number.parseInt(e.target.value));
+        } else if (e.target.value > item.quantity) {
+            setQuantity(item.quantity);
+        } else {
+            setQuantity(1);
         }
     };
 
     const increaseQuantity = () => {
-        setQuantity(quantity+1);
+        if (quantity < item.quantity) {
+            setQuantity(quantity+1);
+        }
     };
 
     const decreaseQuantity = () => {
@@ -36,29 +50,54 @@ const ItemDetails = () => {
         }
     };
 
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios.get("http://localhost:8000/auth/user/", {withCredentials: true})
+        .then(response => {
+            return;
+        })
+        .catch(error => {
+            history.push("/login");
+        });
+
+
+        const url = `http://localhost:8000/shop/items/${id}/`;
+        axios.post(url, {}, {withCredentials: true, params: {quantity}})
+        .then(response => {
+            setResponse(response.data);
+            setError({});
+        })
+        .catch(error => {
+            setResponse({});
+            setError(error.response.data);
+        });
+    };
+
     return (
         <div className="item-details">
             <div className="item-details-images">
                 <img src="https://i.stack.imgur.com/y9DpT.jpg" alt="item" className="item-details-image"/>
             </div>
             <div className="item-details-info">
-                <div className="item-details-name">Name</div>
-                <div className="item-details-description">Description Description Description Description Description
-                Description Description Description Description Description
-                Description Description Description Description Description
-                </div>
+                <div className="item-details-name">{item.name}</div>
+                <div className="item-details-description">{item.description}</div>
                 <div className="item-details-bottom">
-                    <div className="item-quantity">In-stock: 100</div>
-                    <span className="item-price">9.99zl</span>
+                    <div className="item-quantity">In-stock: {item.quantity}</div>
+                    <span className="item-price">{item.price}zl</span>
                 </div>
-                <form className="quantity-form">
-                    <div className="item-quantity">Quantity</div>
-                    <div className="quantity-buttons">
-                        <div onClick={decreaseQuantity} className="sign-box">-</div>
-                        <input type="number" className="quantity-input" value={quantity} onChange={changeQuantity}/>
-                        <div onClick={increaseQuantity} className="sign-box">+</div>
+                {item.is_available || item.quantity <= 0 ? <form className="quantity-form" onSubmit={handleSubmit}>
+                    <div className="quantity-data">
+                        <div className="item-quantity">Quantity: </div>
+                        <div className="quantity-buttons">
+                            <div onClick={decreaseQuantity} className="sign-box">-</div>
+                            <input type="number" className="quantity-input" value={quantity} onChange={changeQuantity}/>
+                            <div onClick={increaseQuantity} className="sign-box">+</div>
+                        </div>
                     </div>
-                </form>
+                    <input type="submit" value="Add to cart" className="add-to-cart"/>
+                </form> : <div className="add-to-cart-unavailable">Item unavailable</div>}
+                {response.detail ? <div className="quantity-success">{response.detail}</div> : null}
+                {error.detail ? <div className="quantity-fail">{error.detail}</div> : null}
             </div>
         </div>
     );
