bc32bd90a955f6036b0fac5fd43722e6ca35c12d
Ansup17174 <ansuported@op.pl>
2021-02-08 20:33:51 +0100

    --amend

--- frontend/src/components/AdminDeleteItem.js
+++ frontend/src/components/AdminDeleteItem.js
@@ -1,5 +1,5 @@
-import {useEffect, useContext} from 'react';
-import {useHistory, useParams} from 'react-router-dom';
+import {useState, useEffect, useContext} from 'react';
+import {useHistory, useParams, Link} from 'react-router-dom';
 import UserContext from '../context/UserContext';
 import axios from 'axios';
 
@@ -7,11 +7,12 @@ const AdminDeleteItem = () => {
     const { id } = useParams();
     const history = useHistory();
     const { reloadUserData } = useContext(UserContext);
+    const [responseOk, setResponseOk] = useState(false);
 
     useEffect(() => {
         axios.delete(`http://localhost:8000/shop/admin/items/${id}/`, {withCredentials: true})
         .then(response => {
-            history.push("/admin-items");
+            setResponseOk(true);
         })
         .catch(error => {
             if (error.response.status === 403) {
@@ -26,7 +27,14 @@ const AdminDeleteItem = () => {
         });
     }, []);
 
-    return null;
+    return (
+        <div className="main-text">
+            {responseOk && <div>
+                <h2>Item deleted succsefully</h2>
+                <Link to="/admin-items"><div className="pay-button">Go back to items</div></Link>
+            </div>}
+        </div>
+    );
 };
 
 export default AdminDeleteItem;
