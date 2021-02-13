d7c86737226afee9a97174993273a8e2f5171eed
Ansup17174 <ansuported@op.pl>
2021-02-13 10:31:53 +0100

    auth service

--- frontend/src/components/user/UserProfile.js
+++ frontend/src/components/user/UserProfile.js
@@ -43,49 +43,49 @@ const UserProfile = () => {
     };
 
     return (
-    <div className="scrollable-page">
-        <div className="form-container">
-            <form className="form" onSubmit={handleSubmit}>
-                <h1>Profile info</h1>
-                {responseOk && <div className="form-success">Profile saved</div>}
-                <div className="form-field">
-                    <div className="form-input">Id: {formData.pk}</div>
-                </div>
-                <div className="form-field">
-                    <div className="form-input">E-mail: {formData.email}</div>
-                </div>
-                {userData.is_staff && <div className="form-field">
-                    <div className="form-input">Is Admin: True</div>
-                </div>}
-                <div className="form-field">
-                    First name: <input type="text" className="form-input" value={formData.first_name} placeholder="First name" onChange={e => setFormData({...formData, first_name: e.target.value})}/>
-                    {errors.first_name && errors.first_name.map((message, index) => <div className="form-error" key={index}>{message}</div>) }
-                </div>
-                <div className="form-field">
-                    Last name: <input type="text" className="form-input" value={formData.last_name} placeholder="Last name" onChange={e => setFormData({...formData, last_name: e.target.value})}/>
-                    {errors.last_name && errors.last_name.map((message, index) => <div className="form-error" key={index}>{message}</div>)}
-                </div>
-                <div className="form-field">
-                    Phone: <input type="text" className="form-input" value={formData.phone} placeholder="Phone number" onChange={e => setFormData({...formData, phone: e.target.value})}/>
-                    {errors.phone && errors.phone.map((message, index) => <div className="form-error" key={index}>{message}</div>)}
-                </div>
-                <div className="form-field">
-                    Date of birth: <input type={dateInputType}
-                    className="form-input"
-                    value={formData.date_of_birth}
-                    placeholder="Date of birth"
-                    onFocus={() => setDateInputType("date")}
-                        onBlur={() => setDateInputType("text")}
-                        onChange={e => setFormData({...formData, date_of_birth: e.target.value})}/>
-                    {errors.date_of_birth && errors.date_of_birth.map((message, index) => <div className="form-error" key={index}>{message}</div>)}
-                </div>
-                <div className="form-field">
-                    {errors.non_field_errors && errors.non_field_errors.map((message, index) => <div className="form-error" key={index}>{message}</div>)}
-                </div>
-                <input className="form-button form-input" type="submit" value="Save profile"/>
-            </form>
+        <div className="scrollable-page">
+            <div className="form-container">
+                <form className="form" onSubmit={handleSubmit}>
+                    <h1>Profile info</h1>
+                    {responseOk && <div className="form-success">Profile saved</div>}
+                    <div className="form-field">
+                        <div className="form-input">Id: {formData.pk}</div>
+                    </div>
+                    <div className="form-field">
+                        <div className="form-input">E-mail: {formData.email}</div>
+                    </div>
+                    {userData.is_staff && <div className="form-field">
+                        <div className="form-input">Is Admin: True</div>
+                    </div>}
+                    <div className="form-field">
+                        First name: <input type="text" className="form-input" value={formData.first_name} placeholder="First name" onChange={e => setFormData({...formData, first_name: e.target.value})}/>
+                        {errors.first_name && errors.first_name.map((message, index) => <div className="form-error" key={index}>{message}</div>) }
+                    </div>
+                    <div className="form-field">
+                        Last name: <input type="text" className="form-input" value={formData.last_name} placeholder="Last name" onChange={e => setFormData({...formData, last_name: e.target.value})}/>
+                        {errors.last_name && errors.last_name.map((message, index) => <div className="form-error" key={index}>{message}</div>)}
+                    </div>
+                    <div className="form-field">
+                        Phone: <input type="text" className="form-input" value={formData.phone} placeholder="Phone number" onChange={e => setFormData({...formData, phone: e.target.value})}/>
+                        {errors.phone && errors.phone.map((message, index) => <div className="form-error" key={index}>{message}</div>)}
+                    </div>
+                    <div className="form-field">
+                        Date of birth: <input type={dateInputType}
+                        className="form-input"
+                        value={formData.date_of_birth}
+                        placeholder="Date of birth"
+                        onFocus={() => setDateInputType("date")}
+                            onBlur={() => setDateInputType("text")}
+                            onChange={e => setFormData({...formData, date_of_birth: e.target.value})}/>
+                        {errors.date_of_birth && errors.date_of_birth.map((message, index) => <div className="form-error" key={index}>{message}</div>)}
+                    </div>
+                    <div className="form-field">
+                        {errors.non_field_errors && errors.non_field_errors.map((message, index) => <div className="form-error" key={index}>{message}</div>)}
+                    </div>
+                    <input className="form-button form-input" type="submit" value="Save profile"/>
+                </form>
+            </div>
         </div>
-    </div>
     );
 };
 
