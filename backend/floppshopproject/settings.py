1ff29a08141ccafad899132107edbd8f4319e631
Ansup17174 <ansuported@op.pl>
2021-02-13 16:31:14 +0100

    prepared to deploy

--- backend/floppshopproject/settings.py
+++ backend/floppshopproject/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -175,7 +176,7 @@ JWT_AUTH_COOKIE = 'floppauth'
 OLD_PASSWORD_FIELD_ENABLED = True
 JWT_AUTH_SAMESITE = 'Strict'
 LOGOUT_ON_PASSWORD_CHANGE = False
-JWT_AUTH_SECURE = False
+JWT_AUTH_SECURE = True
 
 
 # rest_framework_simplejwt
@@ -237,7 +238,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifeststaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
