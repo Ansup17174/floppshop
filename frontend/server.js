9829b569abb7684241f7ba62c6bcd5e662a1edaa
Ansup17174 <ansuported@op.pl>
2021-02-13 20:19:12 +0100

    --amend

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const favicon = require('serve-favicon');
 const path = require('path');
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 
