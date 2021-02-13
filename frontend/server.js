97090eac19767b208a37a16386c646cafd64ca55
Ansup17174 <ansuported@op.pl>
2021-02-13 18:23:55 +0100

    bruh

--- frontend/server.js
+++ frontend/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const favicon = require('express-favicon');
+const favicon = require('serve-favicon');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build', 'index.html')))
 
