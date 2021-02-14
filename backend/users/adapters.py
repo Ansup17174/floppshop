27de7016217fa8ed659912ddca33b4b2c83aba43
Ansup17174 <ansuported@op.pl>
2021-02-14 12:10:23 +0100

    --amend

--- backend/users/adapters.py
+++ backend/users/adapters.py
@@ -6,7 +6,7 @@ import os
 class CustomAccountAdapter(DefaultAccountAdapter):
 
     def get_email_confirmation_url(self, request, emailconfirmation):
-        url = f"http://{os.environ.get('FRONTEND_HOST')}/verify-email/{emailconfirmation.key}/"
+        url = f"{os.environ.get('FRONTEND_HOST')}/verify-email/{emailconfirmation.key}/"
         return url
 
     def save_user(self, request, user, form, commit=True):
