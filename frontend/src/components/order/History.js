298fa184d007ad24a8a8ec063404573b86a2fd55
Ansup17174 <ansuported@op.pl>
2021-02-15 10:41:01 +0100

    history fix

--- frontend/src/components/order/History.js
+++ frontend/src/components/order/History.js
@@ -7,19 +7,7 @@ import HistoryDetails from './HistoryDetails';
 import './order.css';
 
 const History = () => {
-    const [selectedOrder, setSelectedOrder] = useState({
-        id: "",
-        carts: [],
-        address: "",
-        method: {},
-        total_price: "",
-        is_finished: false,
-        is_paid: false,
-        date_created: "",
-        date_finished: "",
-        date_paid: "",
-        quantity: 0
-    });
+    const [selectedOrder, setSelectedOrder] = useState({});
     const [orders, setOrders] = useState([]);
     const {reloadUserData} = useContext(UserContext);
     const [page, setPage] = useState(1);
@@ -37,7 +25,11 @@ const History = () => {
     const getOrderHistory = () => {
         apiInstance.get(`shop/order/?history&limit=10&offset=${(page-1)*10}`, {withCredentials: true})
         .then(response => {
-            setOrders(response.data.results);
+            if (response.data.results) {
+                setOrders(response.data.results);
+            } else {
+                setOrders(response.data);
+            }
             setMaxPage(Math.floor((response.data.count-1) / 10) + 1);
         })
         .catch(error => {
@@ -55,16 +47,16 @@ const History = () => {
     return (
         <div className="scrollable-page">
             {!selectedOrder.id && <div className="gray-container">
-                {orders.length > 0 && <><h1>Order history</h1>
+                {orders && orders.length > 0 && <><h1>Order history</h1>
                 <h3>Page: <input type="number" className="filter-form-input page-input white" value={page} onChange={e => changePage(e.target.value)}/> of {maxPage}</h3>
                 <div className="page-buttons">
                     {page !== 1 && <span className="blue-button" onClick={() => changePage(page-1)}>&lt;</span>}
                     {page !== maxPage && <span className="blue-button" onClick={() => changePage(page+1)}>&gt;</span>}
                 </div>
                 </>}
-                {!orders.length > 0 && <h1>No orders</h1>}
+                {orders && orders.length === 0 && <h1>No orders</h1>}
             </div>}
-            {orders.length > 0 && !selectedOrder.id && orders.map((order, index) => <OrderFinished order={order} key={index} selectOrder={setSelectedOrder}/>)}
+            {orders && orders.length > 0 && !selectedOrder.id && orders.map((order, index) => <OrderFinished order={order} key={index} selectOrder={setSelectedOrder}/>)}
             {selectedOrder.id && <HistoryDetails order={selectedOrder} selectOrder={setSelectedOrder}/>}
         </div>
     );
